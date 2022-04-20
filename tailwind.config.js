12d69c0bc2f3d77c413c30ef58a51180b7358f37
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-04-20 14:12:30 -0500

    Finished product

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "black": "#0B132A",
+        "grey": "#4F5665",
+        "primary": "#F53838",
+      },
+      fontFamily: {
+        "sans": "Rubik"
+      },
+      backgroundImage: {
+      },
+    },
+  },
+  plugins: [
+  ],
+}
